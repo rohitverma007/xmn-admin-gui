37fdc3def6659c47adf8403d420a00ecf6c22553
Rohit Verma <rohitverma@live.ca>
2018-11-14 10:13:45 -0800

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <div class="body">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/shared/Navbar'
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.body {
+  margin-top: 50px;
+}
+</style>
